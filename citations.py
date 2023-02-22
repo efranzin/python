74d01a1b605eaa71fdf856ef4ac99e72b825ed5d
efranzin <edgardo.franzin@gmail.com>
2023-02-22 15:29:15 +0100

    Added computation of the h-index

--- citations.py
+++ citations.py
@@ -7,7 +7,7 @@ Inspirehep database (https://inspirehep.net/) for each paper in a given collecti
 """
 
 __author__ = 'Edgardo Franzin'
-__version__ = '2.3'
+__version__ = '2.4'
 __license__ = 'GPL'
 __email__ = 'edgardo<dot>franzin<at>gmail<dot>com'
 
@@ -38,6 +38,9 @@ order = options.order
 
 # Import the modules to open and reading URLs and the JSON encoder
 import requests
+
+# Import numpy
+import numpy as np
 
 # Open the INSPIRE-HEP profile
 inspirehep_profile = 'https://inspirehep.net/api/literature?sort=mostrecent&size=1000&q=a%20' + BAI
@@ -71,6 +74,8 @@ elif collection == 'proceedings':
 # Load the data
 data = requests.get(inspirehep_profile).json()
 total_hits = data['hits']['total']
+data_published = requests.get(inspirehep_profile + '&doc_type=published').json()
+total_hits_published = data_published['hits']['total']
 
 # Sorting: default is from most recent
 hits = range(total_hits)
@@ -80,6 +85,11 @@ if order == True:
 # For each record print the title, the number of citations and the number of citations excluding self cites
 totcits = totcits_noself = totcits_published = totcits_noself_published = 0
 
+# Arrays to compute the h-index for published papers
+hits_array_published = np.arange(1, total_hits_published+1)
+cits_array_published = cits_noself_array_published = np.empty(0,int)
+
+# Print the number of citations and the number of citations excluding self cites for each entry
 for i in hits:
     title = data['hits']['hits'][i]['metadata']['titles'][0]['title']
     cits = data['hits']['hits'][i]['metadata']['citation_count']
@@ -87,13 +97,22 @@ for i in hits:
     if 'refereed' in data['hits']['hits'][i]['metadata']:
         totcits_published = totcits_published + cits
         totcits_noself_published = totcits_noself_published + cits_noself
+        cits_array_published = np.append(cits_array_published,cits)
+        cits_noself_array_published = np.append(cits_noself_array_published,cits_noself)
     print('\033[1m' + title + '\033[0m')
     print('Number of citations: ', cits, '; Excluding self cites: ', cits_noself, sep='')
     totcits = totcits + cits
     totcits_noself = totcits_noself + cits_noself
 
-# Print the total number of citations and the total number of citations excluding self cites
+# Compute the h-index
+if len(cits_array_published) > 0:
+    h_index = np.max(np.minimum(np.sort(cits_array_published)[::-1], hits_array_published))
+    h_index_noself = np.max(np.minimum(np.sort(cits_noself_array_published)[::-1], hits_array_published))
+
+# Print the total number of citations, the total number of citations excluding self cites, and the h-index
 if total_hits > 0:
     print('\nTotal number of citations: ', totcits, '; Excluding self cites: ', totcits_noself, sep='')
+if len(cits_array_published) > 0:
+    print('h-index: ', h_index, '; Excluding self cites: ', h_index_noself, sep='')
 if collection == 'all':
     print('Total number of citations: ', totcits_published, '; Excluding self cites: ', totcits_noself_published, ' (Published only)', sep='')
